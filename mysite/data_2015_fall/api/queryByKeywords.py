67c7e88fe290846e50663eff727821c7629a061a
moumoutsay <weilints@andrew.cmu.edu>
2015-12-02 14:05:09 -0800

    Able to split keywords and than match with article title

--- mysite/data_2015_fall/api/queryByKeywords.py
+++ mysite/data_2015_fall/api/queryByKeywords.py
@@ -1,19 +1,60 @@
 from django.http import JsonResponse
 from data_2015_fall.models import *
 
+# ===================================================
+#	Classes 
+# ===================================================
+'''
+	A class represnet a paper with following fields 
+	title: title of the paper[string]
+	authors: array of authors[string]
+	relevance: how relevant of this paper in terms of search quiteria[int] 
+'''
+class Paper(object):
+    title = ""
+    authors = []
+    relevance = 0
+    def __init__(self, title, authors, relevance):
+        self.title = title
+        self.authors = authors
+        self.relevance = relevance
+
+    def __hash__(self):
+        return hash(self.title)
+
+    def __cmp__(self, other): # TODO, not sure for now 
+        return cmp(self.name, other.name)
+
+    def toDict(self):
+        return {
+            "title": self.title
+        }
+
+# ===================================================
+#	Functions 
+# ===================================================
 def demo_wl(request):
     print "wl"
     return JsonResponse({'weilin': 'tsai'})
 
 def getTopKRelevantPapersWithAuthorsByKeywords(request, keywords, k):
     print "call getTopKRelevantPapersWithAuthorsByKeywords()"
+    keywordsList = keywords.lower().split("+")
+    for word in keywordsList:
+        print word
     cnt = 0
-    for tmp in Article.nodes:
-    	print tmp.title
-    	#print tmp.name
-    	cnt += 1
-    	if (cnt > 10):
+    
+    for article in Article.nodes:
+        lowerTitle = article.title.lower()
+        matchCnt = 0
+        for keyword in keywordsList:
+            if keyword in lowerTitle:
+    	       matchCnt += 1
+    	if (matchCnt > 1):
+            print article.title
+            cnt += 1
+    	if (cnt > 20):
     		break
     print cnt	
-
-    return JsonResponse({keywords: 'k'})
+    
+    return JsonResponse({keywords: k})
