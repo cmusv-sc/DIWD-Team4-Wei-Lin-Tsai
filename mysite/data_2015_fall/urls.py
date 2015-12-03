b81af19eae7cd39cdab09eb10a068b6c4664d213
moumoutsay <weilints@andrew.cmu.edu>
2015-12-02 22:39:32 -0800

    merge getAuthorContributionByAuthorName branch

--- mysite/data_2015_fall/urls.py
+++ mysite/data_2015_fall/urls.py
@@ -6,5 +6,6 @@ from views import *
 urlpatterns = [
     url(r'^coauthors/(?P<level>\d+)/(?P<name>.+)$', findCoAuthorsMultiLevel),
     url(r'^coauthors/(?P<name>.+)$', findCoAuthors),
-    url(r'^papers/(?P<keywords>.+)/(?P<k>\d+)$', getTopKRelevantPapersWithAuthorsByKeywords)
+    url(r'^papers/(?P<keywords>.+)/(?P<k>\d+)$', getTopKRelevantPapersWithAuthorsByKeywords),
+    url(r'^contributions/(?P<name>.+)$', getAuthorContributionToAJournal)
 ]
