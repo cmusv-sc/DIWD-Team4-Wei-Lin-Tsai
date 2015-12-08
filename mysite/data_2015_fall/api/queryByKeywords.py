65ecad7fed6de821090829d6b240392966ad4cf1
moumoutsay <weilints@andrew.cmu.edu>
2015-12-07 16:51:56 -0800

    Add related papers in topK papers at backend side

--- mysite/data_2015_fall/api/queryByKeywords.py
+++ mysite/data_2015_fall/api/queryByKeywords.py
@@ -13,13 +13,14 @@ import simplejson
 	relevance: how relevant of this paper in terms of search quiteria[int] 
 '''
 class Paper(object):
-    def __init__(self, title, authors, relevance, journal, year, volume):
+    def __init__(self, title, authors, relevance, journal, year, volume, related_papers):
         self.title = title
         self.authors = authors
         self.relevance = relevance
         self.journal = journal
         self.year = year
         self.volume = volume
+        self.related_papers = related_papers
 
     def __hash__(self):
         return hash(self.title)
@@ -33,12 +34,17 @@ class Paper(object):
             "authors": [a.toDict() for a in self.authors],
             "journal": self.journal,
             "year": self.year,
-            "volume": self.volume
+            "volume": self.volume, 
+            "related_papers": [p.toDictTitleOnly() for p in self.related_papers]
         }
+
+    def setRelatedPapers(self, related_papers):
+        self.related_papers = related_papers
 
 # ===================================================
 #	Functions 
 # ===================================================
+# sample query: http://127.0.0.1:8000/dblp/papers/Temporal+Semantic+Databases/100
 def getTopKRelevantPapersWithAuthorsByKeywords(request, keywords, k):
     print "call getTopKRelevantPapersWithAuthorsByKeywords()"
     keywordsList = keywords.lower().split("+")
@@ -52,7 +58,7 @@ def getTopKRelevantPapersWithAuthorsByKeywords(request, keywords, k):
             if keyword in lowerTitle:
     	       matchCnt += 1
     	if (matchCnt > 0):
-            pQ.put(Paper(article.title, article.authors, matchCnt, article.journal, article.year, article.volume))
+            pQ.put(Paper(article.title, article.authors, matchCnt, article.journal, article.year, article.volume, article.citations))
         if (pQ.qsize() > int(k) + 5): # remove some item if Queue is too large, 5 for margin because qsize is not precise 
             pQ.get()    
 
@@ -63,6 +69,18 @@ def getTopKRelevantPapersWithAuthorsByKeywords(request, keywords, k):
         papers.append(article)
     papers.reverse()
     papers = papers[0:int(k)]
+
+    # further process
+    for p in papers:
+        refined_related_paper = []
+        for r in p.related_papers:
+            for pp in papers:
+                if pp.title == r.title:
+                    refined_related_paper.append(r)
+        #for rr in refined_related_paper:
+        #    print rr.title
+        p.setRelatedPapers(refined_related_paper)
+
     ''' debug part 
     for p in papers:
         print p.title + " >> " + str(p.relevance) + ":  " +  simplejson.dumps(p.toDict())
