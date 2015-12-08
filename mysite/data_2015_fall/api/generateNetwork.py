187053585f1deacbe02a37dd43fb4e80ec040f0e
Zack Liang <hsuantzl@andrew.cmu.edu>
2015-12-07 19:06:31 -0800

    Add paper_author API, remove unused urls, refactor citation network API

--- mysite/data_2015_fall/api/generateNetwork.py
+++ mysite/data_2015_fall/api/generateNetwork.py
@@ -1,17 +1,17 @@
 from django.http import JsonResponse
 from data_2015_fall.models import *
 from neomodel import db
-import json
 
-class CitationNetworkResponse():
-    def __init__(self, title):
-        self.title = title
-        self.citations = []
 
-    def toDict(self):
+class NetworkResponse():
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def to_dict(self):
         return {
-            "title": self.title,
-            "children": [c.toDict() for c in self.citations],
+            "name": self.name,
+            "children": [c.to_dict() for c in self.children],
         }
 
 
@@ -22,28 +22,38 @@ def get_citations(name):
 
 
 def get_citations_network(request, name):
-    root = CitationNetworkResponse(name)
-    root.citations = [CitationNetworkResponse(title) for title in get_citations(name)]
-    for c in root.citations:
-        c.citations = [CitationNetworkResponse(title) for title in get_citations(c.title)]
-        for c2 in c.citations:
-            c2.citations = [CitationNetworkResponse(title) for title in get_citations(c2.title)]
-    return JsonResponse(root.toDict())
+    root = NetworkResponse(name)
+    root.children = [NetworkResponse(title) for title in get_citations(name)]
+    for c in root.children:
+        c.children = [NetworkResponse(title) for title in get_citations(c.name)]
+        for c2 in c.children:
+            c2.children = [NetworkResponse(title) for title in get_citations(c2.name)]
+    return JsonResponse(root.to_dict())
 
 
-def get_authors(request, name):
+def get_authors(name):
     query = "match (n1)<-[:AUTHORED]-(n2) where n1.title='%s' return n2" % name
     results, meta = db.cypher_query(query)
-    return JsonResponse({"authors": [a.toDict() for a in [Author.inflate(row[0]) for row in results]] })
+    return [a.name for a in [Author.inflate(row[0]) for row in results]]
 
 
-def get_papers(request, name):
+def get_papers(name):
     query = "match (n1)-[:AUTHORED]->(n2) where n1.name='%s' return n2" % name
     results, meta = db.cypher_query(query)
-    return JsonResponse({"papers": [a.toDict() for a in [Article.inflate(row[0]) for row in results]] })
+    return [a.title for a in [Article.inflate(row[0]) for row in results]]
 
 
 def get_coauthors(request, name):
     query = "match (n1)-[:COAUTHORED]->(n2) where n1.name='%s' return n2" % name
     results, meta = db.cypher_query(query)
-    return JsonResponse({"authors": [a.toDict() for a in [Author.inflate(row[0]) for row in results]] })
+    return JsonResponse({"authors": [a.toDict() for a in [Author.inflate(row[0]) for row in results]]})
+
+
+def get_paper_author_network(request, name):
+    root = NetworkResponse(name)
+    root.children = [NetworkResponse(author) for author in get_authors(name)]
+    for author in root.children:
+        author.children = [NetworkResponse(title) for title in get_papers(author.name)]
+        for paper in author.children:
+            paper.children = [NetworkResponse(author) for author in get_authors(paper.name)]
+    return JsonResponse(root.to_dict())
--- mysite/data_2015_fall/urls.py
+++ mysite/data_2015_fall/urls.py
@@ -13,8 +13,7 @@ urlpatterns = [
     url(r'^collaborators/(?P<name>.+)/(?P<keywords>.+)/(?P<k>\d+)$', queryCollaboratorsByNameAndKeyWords),
     url(r'^cited/(?P<name>.+)/(?P<year>\d+)/(?P<k>\d+)$', get_top_k_cited_papers),
     url(r'^citations/(?P<name>.+)$', get_citations_network),
-    url(r'^paper/(?P<name>.+)$', get_authors),
-    url(r'^author/(?P<name>.+)$', get_papers),
+    url(r'^paper_author/(?P<name>.+)$', get_paper_author_network),
     url(r'^journalsdist/(?P<startYear>\d+)/(?P<endYear>\d+)', queryPublicationsBetweenYears),
     url(r'^pubovertime/(?P<name>.+)', queryPubsOfAuthorOverTime),
 ]
